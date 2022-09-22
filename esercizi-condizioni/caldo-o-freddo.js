9335d47780a3e4b8e716c16e3593f892ff939d82
Welkin78 <113534662+Welkin78@users.noreply.github.com>
2022-09-22 16:20:33 +0200

    Update caldo-o-freddo.js
    Buongiorno,
    è possibile avere altri esempi con modi diversi per risolvere il problema? 
    Grazie.

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,54 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let a = 10;
+let b = -2;
+let c = 31;
+let d = 22;
+let e = 15;
+let f = -6;
+let g = 7;
+
+if (a<b && a<c && a<d && a<e && a<f && a<g) {
+  alert('La Temperatura più bassa è: ' + a);
+} else if (a>b && a>c && a>d && a>e && a>f && a>g) {
+    alert('La Temperatura più alta è: '+ a);
+}{
+}
+if (b<a && b<c && b<d && b<e && b<f && b<g) {
+  alert('La Temperatura più bassa è: ' + b);
+} else if (b>a&&b>c&&b>d&&b>e&&b>f&&b>g) {
+    alert('La Temperatura più alta è: '+ b);
+}{
+}
+if (c<a && c<b && c<d && c<e && c<f &&c<g) {
+  alert('La Temperatura più bassa è: ' + c);
+} else if (c>a && c>b && c>d && c>e && c>f && c>g) {
+    alert('La Temperatura più alta è: '+ c);
+}{
+}
+if (d<a && d<b && d<c && d<e && d<f && d<g) {
+  alert('La Temperatura più bassa è: ' + d);
+} else if (d>a && d>b && d>c && d>e && d>f && d>g) {
+    alert('La Temperatura più alta è: '+ d);
+}{
+}
+if (e<a && e<b && e<c && e<d && e<f && e<g) {
+  alert('La Temperatura più bassa è: ' + e);
+} else if (e>a && e>b && e>c && e>d && e>f && e>g) {
+    alert('La Temperatura più alta è: '+ e);
+}{
+}
+if (g<a && g<b && g<c && g<d && g<e && g<f) {
+  alert('La Temperatura più bassa è: ' + g);
+} else if (g>a && g>b && g>c && g>d && g>e && g>f) {
+    alert('La Temperatura più alta è: '+ g);
+}{
+}
+if (f<a && f<b && f<c && f<d && f<e && f<g) {
+  alert('La Temperatura più bassa è: ' + f);
+} else if (f>a && f>b && f>c && f>d && f>e && f>g) {
+    alert('La Temperatura più alta è: '+ f);
+}{
+}
