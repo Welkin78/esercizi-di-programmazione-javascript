88540654a9f3bdfe62f9a4071662f5dffac6917a
Welkin78 <113534662+Welkin78@users.noreply.github.com>
2022-09-23 10:18:33 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,55 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let voto = prompt('Che voto hai preso? ', '');
+
+switch (voto) {
+  case '1':
+  case '2':
+  case '3':
+  case '4':
+  case '5':
+  case '6':
+  case '7':
+  case '8':
+  case '9':
+  case '10':
+  case '11':
+  case '12':
+  case '13':
+  case '14':
+  case '15':
+  case '16':
+  case '17':
+  alert('INSUFFICIENTE');
+    break;
+  case '18':
+  case '19':
+  case '20':
+  alert('SUFFICIENTE');
+    break;
+  case '21':
+  case '22':
+  case '23':
+  alert('BUONO');
+    break;
+  case '24':
+  case '25':
+  case '26':
+  alert('DISTINTO');
+    break;
+  case '27':
+  case '28':
+  case '29':
+  alert('OTTIMO');
+    break;
+  case '30':
+  alert('ECCELLENTE');
+    break;
+  case '0':
+  alert('VOTO NON AMMISSIBILE');
+    break;
+  default:
+  alert('VOTO NON AMMISSIBILE');
+}
